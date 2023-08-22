2e4729c8ac3c4dfa8562e1b6b8e231f21b2bb21c
Psi-2789 <141801385+Psi-2789@users.noreply.github.com>
2023-08-22 15:16:58 +0530

    Update new_task.py

--- new_task.py
+++ new_task.py
@@ -9,8 +9,8 @@ char_frequency = {}
 for char in user_name:
   if char in char_frequency:
     char_frequency[char] += 1
-        else:
-            char_frequency[char] = 1
+    else:
+        char_frequency[char] = 1
 num_duplicates = sum(1 for freq in char_frequency.values() if freq > 1)
 print("Total number of duplicate characters:", num_duplicates)
 
